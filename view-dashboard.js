788a081acd0b098c28f2c219227c7c0d47c5a5cb
Marcin <marcin.straniec@gmail.com>
2022-09-05 23:15:32 +0200

    learning rest api - countries app
    /w tutorial

---
+++ view-dashboard.js
@@ -0,0 +1,45 @@
+import { renderCountrieslist } from "./dom-utils.js";
+export const renderDashboard = () => {
+const API_URL_ALL = 'https://restcountries.com/v3.1/all';
+
+let countries;
+let query = "";
+let region = "";
+
+fetch(API_URL_ALL)
+  .then((res) => res.json())
+  .then((countriesRaw) => {
+      countries = countriesRaw.map((country) => {
+        return {
+          capital: country.capital && country.capital[0],
+          population: country.population.toLocaleString(),
+          name: country.name.common,
+          code: country.cioc,
+          region: country.region,
+          flagUrl: country.flags.png
+    };
+  });
+  renderCountrieslist(countries);
+});
+
+const filterDataAndRenderCountriesList = () => {
+  const filteredCountries = countries.filter((country) => {
+    return (
+      country.name.toLowerCase().includes(query) &&
+      (!region || country.region === region)
+    );
+  });
+
+  renderCountrieslist(filteredCountries);
+};
+
+document.querySelector('#query').addEventListener('input', (e) => {
+  query = e.target.value.toLowerCase().trim();
+  filterDataAndRenderCountriesList();
+});
+
+document.querySelector('#region').addEventListener('change', (e) => {
+  region = e.target.value;
+  filterDataAndRenderCountriesList();
+});
+};
